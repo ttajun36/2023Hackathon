e3a12d08a80237f9672401037815a10621b373a6
ttajun36 <31652115+ttajun36@users.noreply.github.com>
2023-04-29 19:11:10 +0900

    reset

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -112,7 +112,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    child: const Text("Don't have an account? I change login_screen"),
+                    child: const Text("Don't have an account?"),
                     padding: const EdgeInsets.symmetric(
                       vertical: 8,
                     ),
