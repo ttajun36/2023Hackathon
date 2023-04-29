2b1895b8742abe3fc11e2e085e73d5bd825def1a
ttajun36 <31652115+ttajun36@users.noreply.github.com>
2023-04-29 22:26:15 +0900

    editing profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/user_provider.dart';
 
 class ProfileScreen extends StatefulWidget {
   final String uid;
@@ -22,9 +26,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final UserProvider userProvider = Provider.of<UserProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('User Detail'),
+        title: Text('Profile'),
       ),
       body: FutureBuilder(
         future: _user,
@@ -38,48 +44,173 @@ class _ProfileScreenState extends State<ProfileScreen> {
             DocumentSnapshot user = snapshot.data!;
             // 여기에서 user 데이터를 사용하여 UI를 구성합니다.
             return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    // CircleAvatar를 추가합니다.
-                    radius: 65,
-                    backgroundImage: NetworkImage(user['photoUrl']),
-                  ),
-                  SizedBox(height: 20),
-                  Text.rich(
-                    TextSpan(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 250),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
                       children: [
-                        TextSpan(
-                          text: user['username'],
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
+                        Image.network(
+                          user['userCoverImage'],
+                          fit: BoxFit.cover,
+                          height: 250,
+                          width: double.infinity,
                         ),
-                        TextSpan(
-                          text: ' (${user['userage'].toString()})',
-                          style: TextStyle(fontSize: 20),
+                        Transform.translate(
+                          offset: Offset(
+                              0, 40), // 이 값의 y 좌표를 조절하여 프로필 이미지를 아래로 이동시킵니다.
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2.0, // 테두리 두께를 조절하려면 이 값을 변경하세요.
+                              ),
+                            ),
+                            child: CircleAvatar(
+                              radius: 65,
+                              backgroundImage: NetworkImage(user['photoUrl']),
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [                              
-                      Text(
-                        user['usermajor'],
+                    SizedBox(height: 20),
+                    Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: user['username'],
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: ' (${user['userage'].toString()})',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          user['usermajor'],
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          user['userID'].toString(),
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            // 블로그 아이콘을 클릭했을 때 실행되는 코드
+                          },
+                          child: Icon(
+                            FontAwesomeIcons.blog,
+                            size: 24,
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        InkWell(
+                          onTap: () {
+                            // 인스타그램 아이콘을 클릭했을 때 실행되는 코드
+                          },
+                          child: Icon(
+                            FontAwesomeIcons.instagram,
+                            size: 24,
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        InkWell(
+                          onTap: () {
+                            // 유튜브 아이콘을 클릭했을 때 실행되는 코드
+                          },
+                          child: Icon(
+                            FontAwesomeIcons.youtube,
+                            size: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () async {
+                        if (userProvider.getUser.uid == widget.uid) {
+                          TextEditingController descriptionController =
+                              TextEditingController();
+                          descriptionController.text = user['userDescription'];
+
+                          await showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('수정하기'),
+                                content: TextField(
+                                  controller: descriptionController,
+                                  maxLines: 5,
+                                  decoration: InputDecoration(
+                                    labelText: 'Description',
+                                    border: OutlineInputBorder(),
+                                  ),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () async {
+                                      // Firestore 내에 있는 user['userDescription']을 update
+                                      await FirebaseFirestore.instance
+                                          .collection('users')
+                                          .doc(widget.uid)
+                                          .update({
+                                        'userDescription':
+                                            descriptionController.text
+                                      });
+
+                                      // UI 갱신
+                                      setState(() {
+                                        _user = FirebaseFirestore.instance
+                                            .collection('users')
+                                            .doc(widget.uid)
+                                            .get();
+                                      });
+
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Save'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        } else {
+                          print('different');
+                        }
+                      },
+                      child: Text(
+                        user['userDescription'],
                         style: TextStyle(fontSize: 18),
                       ),
-                       SizedBox(width: 10),
-                       Text(
-                        user['userID'].toString(),
-                        style: TextStyle(fontSize: 18),
-                      ),             
-                    ],
-                  ),
-                  // 여기에 추가적인 UI 요소를 배치할 수 있습니다.
-                ],
+                    ),
+                    // 여기에 추가적인 UI 요소를 배치할 수 있습니다.
+                  ],
+                ),
               ),
             );
             // 제목을 표시합니다.
